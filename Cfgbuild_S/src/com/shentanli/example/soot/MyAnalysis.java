bfba701765d7a5901ca543c33e940289f5256830
2048li <shentanli@qq.com>
2016-03-30 03:39:18 -0400

    value

--- Cfgbuild_S/src/com/shentanli/example/soot/MyAnalysis.java
+++ Cfgbuild_S/src/com/shentanli/example/soot/MyAnalysis.java
@@ -1,5 +1,6 @@
 package com.shentanli.example.soot;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.swing.text.html.HTMLDocument.Iterator;
@@ -10,6 +11,8 @@ import soot.Scene;
 import soot.SootClass;
 import soot.SootMethod;
 import soot.Unit;
+import soot.Value;
+import soot.ValueBox;
 
 public class MyAnalysis extends BodyTransformer{
 
@@ -18,24 +21,46 @@ public class MyAnalysis extends BodyTransformer{
 		// TODO Auto-generated method stub
 		for (SootClass c:Scene.v().getApplicationClasses())
 		{
-			//System.out.println("sootclass "+ c);
+			//System.out.println("sootclass "+ c);			
+			
 			for (SootMethod m:c.getMethods())
 			{
 				if (m.isConcrete())
 				//if (!m.isJavaLibraryMethod())
 				{
 					Body b = m.retrieveActiveBody();
+				//	List<ValueBox> vale = b.getDefBoxes();
+					
+				//	List<Value> pararef = b.getParameterRefs();
+				//	String[] y = pararef.toArray(new String[0]);
+				//	int len = y.length;
+				/*	int len = vale.size();
+					for (int i = 0;i<len;i++)
+					{
+						String tmp = vale.get(i).toString();
+						//System.out.println("pararef--"+y[i]);
+						System.out.println("boxvalue ---"+tmp);
+					} */
 					java.util.Iterator<Unit> i = b.getUnits().snapshotIterator();
-					while(i.hasNext())
+			     	while(i.hasNext())
 					{
 						Unit u = i.next();
+						List usebox = u.getUseBoxes();
+					    int len = usebox.size();
+					    
+						for (int count = 0;count<len;count++)
+						{
+							if (usebox.get(count).toString().indexOf("pm install") != -1)
+								System.out.println("usebox value--"+usebox.get(count));
+							//System.out.println("usebox value--"+usebox.get(count));
+						}
 						
-						String substr = "getRuntime";
+					/*	String substr = "getRuntime";
 						if (u.toString().indexOf(substr) != -1)
-							System.out.println("unit contains getpah :"+u.toString());
+							System.out.println("unit contains getpah :"+u.toString());   
 						//System.out.println("unit --"+u.toString());
-					//	else
-					//		System.out.println("no getruntime method");
+						else
+							System.out.println("no getruntime method");*/
 					}
 				}
 			}
